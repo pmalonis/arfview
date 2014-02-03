6dcaf702f2bc9e2dc3413ba397caedd3d61c7776
Peter Malonis <pmalonis@uchicago.edu>
2014-02-02 22:35:10 -0600

    switching to eggmix

--- arfview/mainwin.py
+++ arfview/mainwin.py
@@ -122,13 +122,31 @@ class MainWindow(QtGui.QMainWindow):
 
         #settings panel
         self.settings_panel = settingsPanel()
-        
+        print(self.settings_panel.layout())
+        # self.settings_panel = settingsPanel('Settings', size=(200,1))
+        # self.oscillogram = QtGui.QCheckBox("oscillogram")
+        # self.spectrogram = QtGui.QCheckBox("spectrogram")
+        # self.raster = QtGui.QCheckBox("raster")
+        # self.psth = QtGui.QCheckBox("PSTH")
+        # self.isi = QtGui.QCheckBox("ISI")
+        # self.applyButton=QtGui.QPushButton("apply")
+
+        # ledge = 0
+        # uedge = 0
+
+        # self.settings_panel.addWidget(self.oscillogram, uedge, ledge)
+        # self.settings_panel.addWidget(self.spectrogram, uedge + 1, ledge)
+        # self.settings_panel.addWidget(self.raster, uedge + 2, ledge)
+        # self.settings_panel.addWidget(self.isi, uedge + 3, ledge)
+        # self.settings_panel.addWidget(self.psth, uedge + 4, ledge)
+        # self.settings_panel.addWidget(self.applyButton, uedge + 5, ledge + 1)
+
         # final steps
         self.area = pgd.DockArea()
         tree_dock = pgd.Dock("Tree", size=(200, 1))
         data_dock = pgd.Dock("Data", size=(500, 200))
         attr_table_dock = pgd.Dock("Attributes", size=(200, 1))
-        settings_dock = pgd.Dock('Settings',size = (200,1))
+        settings_dock = pgd.Dock('Settings', size=(300,200))
         self.area.addDock(tree_dock, 'left')
         self.area.addDock(data_dock, 'right')
         self.area.addDock(attr_table_dock, 'bottom', tree_dock)
@@ -139,6 +157,8 @@ class MainWindow(QtGui.QMainWindow):
         data_dock.addWidget(self.data_layout)
         attr_table_dock.addWidget(self.attr_table)
         settings_dock.addWidget(self.settings_panel)
+        self.settings_panel.show()
+        
         self.setCentralWidget(self.area)
         self.setWindowTitle('arfview')
         self.resize(1200, 800)
--- arfview/settingsPanel.py
+++ arfview/settingsPanel.py
@@ -1,29 +1,129 @@
 from PySide import QtGui, QtCore
 import pyqtgraph.dockarea as pgd
+import pyqtgraph as pg
 
-class settingsPanel(QtGui.QDialog):
+class settingsPanel(QtGui.QWidget):
     def __init__(self):
-        super(plotCheckedWindow, self).__init__()
-        self.initUI
+        super(settingsPanel, self).__init__()
+        self.initUI()
 
     def initUI(self):
-        self.oscillogram = QtGui.QCheckBox("oscillogram")
-        self.spectrogram = QtGui.QCheckBox("spectrogram")
-        self.raster = QtGui.QCheckBox("raster")
-        self.psth = QtGui.QCheckBox("PSTH")
-        self.isi = QtGui.QCheckBox("ISI")
+        self.time_axis_box = self.create_time_axis_box()
+        self.oscillogram_box = self.create_oscillogram_box()
+        self.spectrogram_box = self.create_spectrogram_box()
+        self.raster_box = self.create_raster_box()
+        self.psth_box = self.create_psth_box()
+        self.isi_box = self.create_isi_box()
         self.applyButton=QtGui.QPushButton("apply")
-        
-        layout=QtGui.QGridLayout()
 
+
+        layout = QtGui.QVBoxLayout()
+        layout.addWidget(self.time_axis_box)
+        layout.addWidget(self.oscillogram_box)
+        layout.addWidget(self.spectrogram_box)
+        layout.addWidget(self.raster_box)
+        layout.addWidget(self.isi_box)
+        layout.addWidget(self.psth_box)
+        layout.addWidget(self.applyButton)
+
+        self.setLayout(layout)
+
+    def create_time_axis_box(self):
+        self.t_min_edit = QtGui.QLineEdit()
+        self.t_max_edit = QtGui.QLineEdit()
+        self.t_min_edit.setMaximumWidth(100)
+        self.t_max_edit.setMaximumWidth(100)
+        group_box = QtGui.QGroupBox("Time Axis")
+        layout = QtGui.QGridLayout()
+        ledge = 0
+        uedge = 1
+        min_label=QtGui.QLabel('Time Min:')
+        max_label=QtGui.QLabel('Time Max:')
+        layout.addWidget(min_label,uedge-1, ledge)
+        layout.addWidget(max_label,uedge-1, ledge+1)
+        layout.addWidget(self.t_min_edit, uedge, ledge)
+        layout.addWidget(self.t_max_edit, uedge, ledge+1)
+
+        layout.setContentsMargins(0,0,50,10)
+        layout.setColumnStretch(1,30)
+        group_box.setMaximumHeight(70)
+        group_box.setLayout(layout)
+
+        print layout.originCorner()
+        return group_box
+        
+    def create_oscillogram_box(self):
+        self.oscillogram_check=QtGui.QCheckBox("Visible")
+        self.y_min_edit = QtGui.QLineEdit()
+        self.y_max_edit = QtGui.QLineEdit()
+        self.y_min_edit.setMaximumWidth(100)
+        self.y_max_edit.setMaximumWidth(100)
+        group_box = QtGui.QGroupBox("Oscillogram")
+        layout = QtGui.QGridLayout()
         ledge = 0
         uedge = 0
+        min_label=QtGui.QLabel('Y Min:')
+        max_label=QtGui.QLabel('Y Max:')
+        layout.addWidget(self.oscillogram_check, uedge, ledge)
+        layout.addWidget(min_label,uedge+1, ledge)
+        layout.addWidget(max_label,uedge+1, ledge+1)
+        layout.addWidget(self.y_min_edit, uedge+2, ledge)
+        layout.addWidget(self.y_max_edit, uedge+2, ledge+1)
+        layout.setContentsMargins(0,0,50,10)
+        layout.setColumnStretch(1,30)
+        group_box.setMaximumHeight(100)
+        group_box.setLayout(layout)
 
-        layout.addWidget(self.oscillogram, uedge, ledge)
-        layout.addWidget(self.spectrogram, uedge + 1, ledge)
-        layout.addWidget(self.raster, uedge + 2, ledge)
-        layout.addWidget(self.isi, uedge + 3, ledge)
-        layout.addWidget(self.psth, uedge + 4, ledge)
-        layout.addWidget(self.applyButton, uedge + 5, ledge + 1)
-        self.setLayout(layout)
+        return group_box
+
+    def create_spectrogram_box(self):
+        self.spectrogram_check=QtGui.QCheckBox("Visible")
+        self.y_min_edit = QtGui.QLineEdit()
+        self.y_max_edit = QtGui.QLineEdit()
+        self.y_min_edit.setMaximumWidth(100)
+        self.y_max_edit.setMaximumWidth(100)
         
+        group_box = QtGui.QGroupBox("Spectrogram")
+        layout = QtGui.QGridLayout()
+        ledge = 0
+        uedge = 0  
+        layout.addWidget(self.spectrogram_check, uedge, ledge)      
+        group_box.setLayout(layout)
+
+        return group_box
+
+    def create_raster_box(self):
+        self.raster_check=QtGui.QCheckBox("Visible")
+        
+        group_box = QtGui.QGroupBox("Raster")
+        layout = QtGui.QGridLayout()
+        ledge = 0
+        uedge = 0  
+        layout.addWidget(self.raster_check, uedge, ledge)      
+        group_box.setLayout(layout)
+
+        return group_box
+
+    def create_psth_box(self):
+        self.psth_check=QtGui.QCheckBox("Visible")
+        
+        group_box = QtGui.QGroupBox("PSTH")
+        layout = QtGui.QGridLayout()
+        ledge = 0
+        uedge = 0  
+        layout.addWidget(self.psth_check, uedge, ledge)      
+        group_box.setLayout(layout)
+
+        return group_box
+
+    def create_isi_box(self):
+        self.isi_check=QtGui.QCheckBox("Visible")
+        
+        group_box = QtGui.QGroupBox("ISI")
+        layout = QtGui.QGridLayout()
+        ledge = 0
+        uedge = 0  
+        layout.addWidget(self.isi_check, uedge, ledge)      
+        group_box.setLayout(layout)
+
+        return group_box
